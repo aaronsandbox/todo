e4d7c14186587e00a7a1eb3658dba21cca25b97d
AWells0607 <aaron.wells@hideawaypizza.com>
2025-01-22 14:58:26 -0600

    Update Puma file

--- config/puma.rb
+++ config/puma.rb
@@ -24,13 +24,15 @@
 # Any libraries that use a connection pool or another resource pool should
 # be configured to provide at least as many connections as the number of
 # threads. This includes Active Record's `pool` parameter in `database.yml`.
+workers ENV.fetch("WEB_CONCURRENCY", 2).to_i
 threads_count = ENV.fetch("RAILS_MAX_THREADS", 5)
 threads threads_count, threads_count
+
+preload_app!
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 port ENV.fetch("PORT", 3000)
 environment ENV.fetch("RAILS_ENV") { "production" }
-bind "tcp://0.0.0.0:#{ENV.fetch('PORT') { 3000 }}"
 
 # Allow puma to be restarted by `bin/rails restart` command.
 plugin :tmp_restart
